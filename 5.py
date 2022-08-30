070cb3a62945f8981b9c65b2953eee0b39aa6202
vxn90270 <112522166+vxn90270@users.noreply.github.com>
2022-08-30 18:22:25 -0500

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,13 @@
+# Calculate the area of a circle and assign the value to a variable name of _area_of_circle_
+radius = 30
+_area_of_circle_ = 3.14 * radius * radius
+print("Area of circle: ", _area_of_circle_)
+
+# Calculate the circumference of a circle and assign the value to a variable name of _circum_of_circle_
+_circum_of_circle_ = 2 * 3.14 * radius
+print("Circulference of a circle: ", _circum_of_circle_)
+
+# Take radius as user input and calculate the area
+radius_ = int(input("Please input radius: "))
+_area_of_circle_ = 3.14 * radius_ * radius_
+print("Area of circle: ", _area_of_circle_)
---
+++ 7.py
@@ -0,0 +1,2 @@
+# Use a tabe esapce sequence to get the following lines.
+print("Name\tAge\tCountry\tCity\nAsabeneh\t250\tFinland\tHelsinki")
