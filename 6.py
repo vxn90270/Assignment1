23c90687cdadcd95eafa6694b3b760bef7537822
vxn90270 <112522166+vxn90270@users.noreply.github.com>
2022-08-30 18:36:18 -0500

    Delete 6.py

--- 6.py
+++
@@ -1,5 +0,0 @@
-sentence = "I am a teacher and I love to inspire and teach people"
-
-# how many unique words have been used in the sentence? Use the split methods and set to get the unique words.
-words = [n.lower() for n in sentence.split(" ")]
-print("number of unique words: ", len(list(set(words))))
