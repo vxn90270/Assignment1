2f6d695e390e352ec2d07f06a4378a568adbb085
vxn90270 <112522166+vxn90270@users.noreply.github.com>
2022-08-30 18:36:39 -0500

    Delete 8.py

--- 8.py
+++
@@ -1,5 +0,0 @@
-# Use the string formatting method to display the following
-radius = 10
-area = 3.14 * radius ** 2
-
-print(f"The area of a circle with radius {radius} is {int(area)} meters square.")
